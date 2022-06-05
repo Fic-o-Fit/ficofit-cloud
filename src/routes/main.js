fa53e9a8307bc879f30a984e5443226c6e0463f1
Adi Purnomo <dev.adipurnomo@gmail.com>
2022-06-05 04:46:55 +0000

    package: update express-validator

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
 const userModel = require("../model/userModel");
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
